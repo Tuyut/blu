75cb5a7aaf35266b31cc4eae583da3657e8c202e
Olivier Devoisin <devoxi@users.noreply.github.com>
2017-06-16 22:20:03 +0200

    add ping endpoint

--- hello.go
+++ hello.go
@@ -7,12 +7,17 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world!\n")
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "pong\n")
 }
 
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf("Listening on http://localhost:8080")
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/ping", ping)
 	http.ListenAndServe(":8080", nil)
 }
